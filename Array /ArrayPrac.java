cc198f37f3189f308f6e29012cf5cfdd0dc7ef89
kathirvel-s-dev <skathirvel213@gmail.com>
2026-06-30 19:47:20 +0530

    array prac

---
+++ Array /ArrayPrac.java
@@ -0,0 +1,15 @@
+public class ArrayPrac{
+    public static void main(String[] args){
+
+        int[] numbers={10,20,30,40,50};
+
+        // System.out.println(numbers[0]);
+
+        for (int i=0;i<numbers.length;i++){  // 0 1 2 3 4
+            if (numbers[i]>30 ){
+                System.out.println(numbers[i]);
+            }
+            
+        }
+    }
+}
