fd5aa50005d267bff1873bcf2ac83cc0e1ea18bd
kathirvel-s-dev <skathirvel213@gmail.com>
2026-07-15 21:38:49 +0530

    arraylist in collection

---
+++ ArrayList/ArrayListCollec.java
@@ -0,0 +1,14 @@
+package ArrayListCollection;
+
+import java.util.ArrayList;
+
+public class ArrayListCollec {
+	public static void main(String[] args) {
+		ArrayList al=new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.addLast(30);
+		al.addFirst(10);
+		System.out.println(al);
+	}
+}
